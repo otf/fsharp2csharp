bca908e45191e82e6a0fc019eb6512350c94ef77
otf <phoenixsingleton@gmail.com>
2013-08-19 20:12:39 +0900

    add code of maybe monad

--- fsharp2csharp/CSharpCode/Program.cs
+++ fsharp2csharp/CSharpCode/Program.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LangExt;
 
 namespace CSharpCode
 {
     class Program
     {
+        public static void PlayMaybeMonad()
+        {
+            var mNone = Option.None;
+            var mRed = Option.Some("red");
+            var mGreen = Option.Some("green");
+
+            Func<string, string, string> plusColor = (color1, color2) => color1 + " " + color2;
+
+            // can't
+            // plusColor(mRed, mGreen);
+            var successResult =
+                    from red in mRed
+                    from green in mGreen
+                    select plusColor(red, green);
+
+            Console.WriteLine(successResult);
+
+            var failResult =
+                from red in mRed
+                from green in mGreen
+                from none in mNone
+                select plusColor(red, green);
+
+            Console.WriteLine(failResult);
+        }
+
+
         static void Main(string[] args)
         {
+            PlayMaybeMonad();
         }
     }
 }
--- fsharp2csharp/FSharpCode/Program.fs
+++ fsharp2csharp/FSharpCode/Program.fs
@@ -1,7 +1,31 @@
 ﻿open Prelude
 
 
+let playMaybeMonad () =
+  let none = None
+  let mRed = Some "red"
+  let mGreen = Some "green"
+
+  let plusColor = fun c1 c2 -> c1 + " " + c2
+
+  let successResult = do' {
+    let! red = mRed
+    let! green = mGreen
+    return plusColor red green
+  }
+
+  printfn "%A" successResult
+
+  let failResult = do' {
+    let! red = mRed
+    let! green = mGreen
+    let! none = none
+    return plusColor red green
+  }
+
+  printfn "%A" failResult
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // 整数の終了コードを返します
+  playMaybeMonad () 
+  0
